b067a83d35a6d9fc7adb2a5d4049182835bf0994
bosskai <425065178@qq.com>
2022-06-19 10:21:40 +0800

    为06命令模式/01服务器接受协议.py增加注释

--- 06命令模式/01服务器接受协议.py
+++ 06命令模式/01服务器接受协议.py
@@ -1,5 +1,11 @@
 from abc import ABCMeta, abstractmethod
+"""
+命令模式（Command Pattern）是一种数据驱动的设计模式，它属于行为型模式。
+请求以命令的形式包裹在对象中，并传给调用对象。调用对象寻找可以处理该命令的合适的对象，
+并把该命令传给相应的对象，该对象执行命令。
 
+认为是命令的地方都可以使用命令模式，比如： 1、GUI 中每一个按钮都是一条命令。 2、模拟 CMD。
+"""
 
 class HandleClientProtocol:
     # Several commands to be executed are encapsulated in protocols
